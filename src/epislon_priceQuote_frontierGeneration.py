18ee0ca7ccd25ae9395abe265bd00235fc8e0527
Haoming Gong <hg343@ilab1.cs.rutgers.edu>
2025-02-27 11:26:00 -0500

    new tasks, policy head finetuning

--- src/epislon_priceQuote_frontierGeneration.py
+++ src/epislon_priceQuote_frontierGeneration.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import sys 
-from gurobipy import Model, GRB
+from mechanism_solver import mechanism_solver_combo, mechanism_solver_single
+
 '''
 The script provides a class financial_option_market that implements the following main functionalities:
 1. epsilon_priceQuote: generate the frontier of options with epsilon price quote
@@ -19,6 +20,8 @@ class Market:
         input_format: the format of the market data, either 'option_series' or 'order format'   
         mechanism_solver: the mechanism solver to compute the profit of the given market. 
         If one wants to customized mechanism solver, one just need to ensure the input to the mechanism solver takes in orders in pandas dataframe format, and returns profit as first output.
+        
+        Strikes: market will prepare unique strikes for orders in the market and 0, infinity for constraints
         '''
         assert isinstance(opt_df, pd.DataFrame), "opt_df must be a pandas dataframe"
         self.opt_df = opt_df 
@@ -46,21 +49,67 @@ class Market:
         else:
             return self.mechanism_solver(orders)[0]
     
-    def priceQuote(self, orders : pd.DataFrame, offset: bool = False):
+    def priceQuote(self, order : pd.DataFrame, offset: bool = True, epsilon: bool = False):
         '''
         Generate the price of of givne input order w.r.t orders in the market
         '''
         market_orders = self.get_market_data_order_format()
-        if offset:
-            pass 
+        if order.loc[0, 'transaction_type'] == 1:
+            # quoting price for buy order, we want to quote price by adding a sell order with premium = 0 to the sell side of the market 
+    
+            new_sell_order = order.copy()
+            new_sell_order.loc[:, 'transaction_type'] = 0
+            new_sell_order.loc[:, 'B/A_price'] = 0
+            market_orders = pd.concat([market_orders, order, new_sell_order], ignore_index=False)
+            objVal = self.apply_mechanism(market_orders, offset, epsilon)
+            return objVal
+        elif order.loc[0, 'transaction_type'] == 0:
+            # quoting price for sell order, we want to quote price by adding a buy order with premium = max price to the buy side of the market 
+            new_buy_order = order.copy()
+            new_buy_order.loc[:, 'transaction_type'] = 1
+            new_buy_order.loc[:, 'B/A_price'] = sys.maxsize
+            market_orders = pd.concat([market_orders, new_buy_order, order], ignore_index=False)
+            objVal = self.apply_mechanism(market_orders, offset, epsilon)
+            return sys.maxsize - objVal
         else:
-            pass 
-
-    def frontierGeneration(self, orders : pd.DataFrame):
+            raise ValueError("Invalid transaction type")
+    def frontierGeneration(self, orders : pd.DataFrame, epsilon: bool = False):
         '''
         Generate the frontier of options with epsilon price quote and constraints
         '''
-        pass
+        # we dont want to change the original orders, so we make a copy of it 
+        orders_copy = orders.copy()
+        frontier_labels = pd.Series(None, index=orders_copy.index)
+        for index, row_series in orders_copy.iterrows():
+            temp_orders = orders_copy.copy()
+            original_index = index 
+            order = row_series.to_frame().T
+            order.index = [original_index]
+            temp_orders.drop(index, inplace=True)
+            quote_price = self.priceQuote(order, temp_orders, epsilon)
+            if row_series['belongs_to_frontier'] == 1:
+                # this is bid order 
+                if quote_price > row_series['B/A_price']:
+                    frontier_labels[original_index] = 1
+                else:
+                    frontier_labels[original_index] = 0
+            else:
+                if quote_price < row_series['B/A_price']:
+                    frontier_labels[original_index] = 1
+                else:
+                    frontier_labels[original_index] = 0
+        orders['belongs_to_frontier'] = frontier_labels
+
+        return orders
+    
+    def update_orders(self, orders : pd.DataFrame):
+        '''
+        Update the orders in the market
+        '''
+        self.opt_order = orders
+        self.strikes = list(set(self.opt_order.loc[:, 'Strike Price of the Option Times 1000']))
+        self.strikes.append(0)
+        self.strikes.append(sys.maxsize)
 
 
     def drop_index(self, indices_to_drop):
@@ -151,203 +200,3 @@ class Market:
 
 
 
-def mechanism_solver_single(orders : pd.DataFrame):
-    '''
-    Solve the mechanism solver given single security orders
-    '''
-
-    
-    # Convert orders to numpy array for processing
-    buy_book = orders[orders['transaction_type'] == 1]
-    sell_book = orders[orders['transaction_type'] == 0]
-    
-    if len(buy_book) == 0 or len(sell_book) == 0:
-        return 0, 0, 0, 0, 0  # Return zeros if no matching possible
-    
-    # Extract option data
-    option_num = len(orders)
-    opt = orders[['C=Call, P=Put', 'Strike Price of the Option Times 1000', 'B/A_price', 'transaction_type']].to_numpy()
-    
-    # Get unique strikes for constraints
-    strikes = orders['Strike Price of the Option Times 1000'].unique()
-    
-    # Create optimization model
-    model = Model("match")
-    model.setParam('OutputFlag', False)
-    
-    # Decision variables
-    gamma = model.addVars(1, option_num, lb=0, ub=1)  # sell to buys
-    delta = model.addVars(1, option_num, lb=0, ub=1)  # buy from asks
-    
-    # Add arbitrage constraints for each strike price
-    opt_l = 1  # Use slack variable for constraints
-    if opt_l == 1:
-        l = model.addVars(1, 1, lb=-GRB.INFINITY, ub=GRB.INFINITY)
-        
-    for strike in sorted(strikes):
-        if opt_l == 1:
-            model.addLConstr(
-                sum(gamma[0,i]*max(opt[i, 0]*(strike-opt[i, 1]), 0) for i in range(option_num) if opt[i, 3] == 1) - 
-                sum(delta[0,i]*max(opt[i, 0]*(strike-opt[i, 1]), 0) for i in range(option_num) if opt[i, 3] == 0) - 
-                l[0,0], GRB.LESS_EQUAL, 0
-            )
-        else:
-            model.addLConstr(
-                sum(gamma[0,i]*max(opt[i, 0]*(strike-opt[i, 1]), 0) for i in range(option_num) if opt[i, 3] == 1) - 
-                sum(delta[0,i]*max(opt[i, 0]*(strike-opt[i, 1]), 0) for i in range(option_num) if opt[i, 3] == 0),
-                GRB.LESS_EQUAL, 0
-            )
-    
-    # Set objective function to maximize profit
-    if opt_l == 1:
-        model.setObjective(
-            sum(gamma[0,i]*opt[i, 2] for i in range(option_num) if opt[i, 3] == 1) - 
-            sum(delta[0,i]*opt[i, 2] for i in range(option_num) if opt[i, 3] == 0) - 
-            l[0,0], GRB.MAXIMIZE
-        )
-    else:
-        model.setObjective(
-            sum(gamma[0,i]*opt[i, 2] for i in range(option_num) if opt[i, 3] == 1) - 
-            sum(delta[0,i]*opt[i, 2] for i in range(option_num) if opt[i, 3] == 0),
-            GRB.MAXIMIZE
-        )
-    
-    # Solve the model
-    model.optimize()
-    
-    # Return results
-    if model.status == GRB.OPTIMAL:
-        profit = model.objVal
-        return profit, 0, profit, 0, 0  # Match format from training.py
-    else:
-        return 0, 0, 0, 0, 0  # Return zeros if no solution found
-
-
-def mechanism_solver_combo(opt_buy_book : pd.DataFrame, opt_sell_book : pd.DataFrame, s1='S1', s2='S2', debug=0):
-	'''
-	opt_buy_book: pandas dataframe contains bid orders; specify whether code requires standarizing this variable
-	opt_sell_book: pandas dataframe contains ask orders;
-	s1: stock 1 name
-	s2: stock 2 name
-	order book: contains coefficients up to len(stock_list); call/put; strike; buy/sell; price (bid/ask)
-
-	debug: whether to debug
-	'''
-	buy_book_index = opt_buy_book.index
-	sell_book_index = opt_sell_book.index
-	sorted_columns_order = ['option1', 'option2','C=Call, P=Put',
-                'Strike Price of the Option Times 1000',
-                'transaction_type', 'B/A_price']
-	opt_buy_book = opt_buy_book[sorted_columns_order].to_numpy()
-	opt_sell_book = opt_sell_book[sorted_columns_order].to_numpy()
-	num_buy, num_sell, num_stock = len(opt_buy_book), len(opt_sell_book), len(opt_buy_book[0])-4
-	# add initial constraints
-
-	f_constraints = []
-	f_constraints.append(np.maximum(opt_buy_book[:, -4]*(np.concatenate(np.matmul(opt_buy_book[:, :-4], np.zeros((num_stock, 1))))-opt_buy_book[:, -3]), 0))
-	f_constraints.append(np.maximum(opt_buy_book[:, -4]*(np.concatenate(np.matmul(opt_buy_book[:, :-4], sys.maxsize*np.ones((num_stock, 1))))-opt_buy_book[:, -3]), 0))
-	g_constraints = []
-	g_constraints.append(np.maximum(opt_sell_book[:, -4]*(np.concatenate(np.matmul(opt_sell_book[:, :-4], np.zeros((num_stock, 1))))-opt_sell_book[:, -3]), 0))
-	g_constraints.append(np.maximum(opt_sell_book[:, -4]*(np.concatenate(np.matmul(opt_sell_book[:, :-4], sys.maxsize*np.ones((num_stock, 1))))-opt_sell_book[:, -3]), 0))
-	sub_obj = 1
-
-	try:
-		# prime problemsys
-		model = Model("match")
-		model.setParam('OutputFlag', False)
-		gamma = model.addVars(1, num_buy, ub=1) #sell to bid orders
-		delta = model.addVars(1, num_sell, ub=1) #buy from ask orders
-		L = model.addVars(1, 1, lb=-GRB.INFINITY, ub=GRB.INFINITY)
-		# constraint of 0
-		buy_sum = sum(delta[0,i]*g_constraints[0][i] for i in range(num_sell))
-		sell_sum = sum(gamma[0,i]*f_constraints[0][i] for i in range(num_buy))
-		model.addLConstr(sell_sum-buy_sum-L[0,0], GRB.LESS_EQUAL, 0)
-		# define obj
-		expense = sum(delta[0,i]*opt_sell_book[i, -1] for i in range(num_sell))
-		gain = sum(gamma[0,i]*opt_buy_book[i, -1] for i in range(num_buy))
-		model.setObjective(gain-expense-L[0,0], GRB.MAXIMIZE)
-
-		# sub problem
-		sub_model = Model("sub_match")
-		sub_model.setParam('OutputFlag', False)
-		M = 1000000
-		s = sub_model.addVars(1, num_stock)
-		f = sub_model.addVars(1, num_buy, lb=-GRB.INFINITY)
-		g = sub_model.addVars(1, num_sell)
-		I = sub_model.addVars(1, num_buy, vtype=GRB.BINARY)
-		for i in range(num_sell):
-			sub_model.addLConstr(opt_sell_book[i, -4]*(sum(opt_sell_book[i, j]*s[0,j] for j in range(num_stock))-opt_sell_book[i, -3])-g[0, i], GRB.LESS_EQUAL, 0)
-		for i in range(num_buy):
-			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])+M*(1-I[0, i])-f[0, i], GRB.GREATER_EQUAL, 0)
-			sub_model.addLConstr(M*I[0, i]-f[0, i], GRB.GREATER_EQUAL, 0)
-			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])+M*(1-I[0, i]), GRB.GREATER_EQUAL, 0)
-			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])-M*I[0, i], GRB.LESS_EQUAL, 0)
-
-		it = 0
-		start = timeit.default_timer()
-		while sub_obj > 0.0005:
-			# add newly generated constraint
-			buy_sum_new = sum(delta[0,i]*g_constraints[-1][i] for i in range(num_sell))
-			sell_sum_new = sum(gamma[0,i]*f_constraints[-1][i] for i in range(num_buy))
-			model.addLConstr(sell_sum_new-buy_sum_new-L[0,0], GRB.LESS_EQUAL, 0)
-			model.optimize()
-			# for v in model.getVars():
-			# 	print('%s %g' % (v.varName, v.x))
-			# save decision variables from prime problem
-			gamma_val = np.array([max(gamma[0, i].x, 0) for i in range(num_buy)])
-			delta_val = np.array([max(delta[0, i].x, 0) for i in range(num_sell)])
-			L_val = L[0,0].x
-			if debug == 2:
-				print(gamma_val)
-				print(delta_val)
-				print(L_val)
-
-			# define sub obj
-			sub_model.setObjective(sum(gamma_val[i]*f[0, i] for i in range(num_buy))-sum(delta_val[i]*g[0, i] for i in range(num_sell))-L_val, GRB.MAXIMIZE)
-			sub_model.optimize()
-			# for v in sub_model.getVars():
-			# 	print('%s %g' % (v.varName, v.x))
-			if debug > 0:
-				if it % 100 == 0:
-					print([s[0, i].x for i in range(num_stock)])
-					print('{}: objective is {} > 0'.format(it, sub_model.objVal))
-				if debug == 2:
-					for i in range(num_buy):
-						print('I:', I[0, i].x)
-						print('f:', f[0, i].x)
-					for i in range(num_sell):
-						print('g:', g[0, i].x)
-			# save decision variables from sub problem			
-			f_constraints.append(np.array([f[0, i].x for i in range(num_buy)]))
-			g_constraints.append(np.array([g[0, i].x for i in range(num_sell)]))
-			sub_obj = sub_model.objVal
-			it += 1
-
-		stop = timeit.default_timer()
-		time = stop - start
-		# print matching result
-		if debug == 1:
-			revenue = 0
-			for i in range(num_buy):
-				if gamma[0, i].x > 0:
-					revenue += gamma[0,i].x * opt_buy_book[i, -1]
-					print('Sell {} to {}({}{}+{}{},{}) at bid price {}'.format(round(gamma[0,i].x, 4), 'C' if opt_buy_book[i, -4]==1 else 'P', \
-						opt_buy_book[i, 0], s1, opt_buy_book[i, 1], s2, opt_buy_book[i, -3], opt_buy_book[i, -1]))
-			for i in range(num_sell):
-				if delta[0, i].x > 0:
-					revenue -= delta[0,i].x * opt_sell_book[i, -1]
-					print('Buy {} from {}({}{}+{}{},{}) at ask price {}'.format(round(delta[0,i].x, 4), 'C' if opt_sell_book[i, -4]==1 else 'P', \
-						opt_sell_book[i, 0], s1, opt_sell_book[i, 1], s2, opt_sell_book[i, -3], opt_sell_book[i, -1]))
-			print('Revenue at T0 is {}; L is {}; Objective is {} = {}'.format(round(revenue,2), round(L[0,0].x, 2), round(revenue-L[0,0].x, 2), round(model.objVal, 2)))
-	except GurobiError as e:
-		print('Error code ' + str(e.errno) + ": " + str(e))
-	except AttributeError:
-		print('Encountered an attribute error')
-	
-	if model is None:
-		return time, 0, 0
-	isMatch = any(delta[0,i].x > 0 for i in range(len(delta))) or any(gamma[0,j].x > 0 for j in range(len(gamma)))
-	matched_stock = {'buy_book_index': None, 'sell_book_index': None}
-	matched_stock['buy_book_index'] = [buy_book_index[i] for i in range(len(gamma)) if gamma[0, i].x > 0]
-	matched_stock['sell_book_index'] = [sell_book_index[i] for i in range(len(delta)) if delta[0, i].x > 0]
-	return time, model.NumConstrs, model.objVal, isMatch, matched_stock
---
+++ src/mechanism_solver.py
@@ -0,0 +1,214 @@
+from gurobipy import Model, GRB
+import pandas as pd
+import numpy as np
+import sys
+from utils import Market 
+
+def mechanism_solver_single(market: Market, offset : bool = True, epsilon: bool = False):
+    '''
+    Solve the mechanism solver given single security orders
+	the market need to set the index of the 
+    '''
+    opt_l = offset # offset is 1 if offset is true, 0 if offset is false
+    orders = market.get_market_data_order_format()
+    # Convert orders to numpy array for processing
+    buy_book = orders[orders.loc[:, 'transaction_type'] == 1]
+    sell_book = orders[orders.loc[:, 'transaction_type'] == 0]
+    
+    if len(buy_book) == 0 or len(sell_book) == 0:
+        return None, None
+    
+    # Extract option data
+    option_num = len(orders)
+    call_or_put = 0
+    strike_price = 1
+    premium = 2
+    opt_buy_book = orders[['C=Call, P=Put', 'Strike Price of the Option Times 1000', 'B/A_price']].to_numpy()
+    opt_sell_book = orders[['C=Call, P=Put', 'Strike Price of the Option Times 1000', 'B/A_price']].to_numpy()
+    # Get unique strikes for constraints
+	#market will prepare unique strikes for orders in the market and 0, infinity for constraints
+    strikes = market.get_strikes() 
+    
+    # Create optimization model
+    model = Model("match")
+    model.setParam('OutputFlag', False)
+    
+    # Decision variables
+    if epsilon:
+        gamma = model.addVars(1, len(opt_buy_book), lb=0, ub=GRB.INFINITY)  # sell to buys
+        delta = model.addVars(1, len(opt_sell_book), lb=0, ub=GRB.INFINITY)  # buy from asks
+    else:
+        gamma = model.addVars(1, len(opt_buy_book), lb=0, ub=1)  # sell to buys
+        delta = model.addVars(1, len(opt_sell_book), lb=0, ub=1)  # buy from asks
+    
+    # Add arbitrage constraints for each strike price
+    if opt_l == 1:
+        l = model.addVars(1, 1, lb=-GRB.INFINITY, ub=GRB.INFINITY)
+        
+    for strike in sorted(strikes):
+        if opt_l == 1:
+            model.addLConstr(
+                sum(gamma[0,i]*max(opt_buy_book[i, call_or_put]*(strike-opt_buy_book[i, strike_price]), 0) for i in range(option_num) ) - 
+                sum(delta[0,i]*max(opt_sell_book[i, call_or_put]*(strike-opt_sell_book[i, strike_price]), 0) for i in range(option_num)	) - 
+                l[0,0], GRB.LESS_EQUAL, 0
+            )
+        else:
+            model.addLConstr(
+                sum(gamma[0,i]*max(opt_buy_book[i, call_or_put]*(strike-opt_buy_book[i, strike_price]), 0) for i in range(option_num) ) - 
+                sum(delta[0,i]*max(opt_sell_book[i, call_or_put]*(strike-opt_sell_book[i, strike_price]), 0) for i in range(option_num)),
+                GRB.LESS_EQUAL, 0
+            )
+    
+    # Set objective function to maximize profit
+    if opt_l == 1:
+        model.setObjective(
+            sum(gamma[0,i]*opt_buy_book[i, premium] for i in range(option_num) ) - 
+            sum(delta[0,i]*opt_sell_book[i, premium] for i in range(option_num)),
+            GRB.MAXIMIZE
+        )
+    else:
+        model.setObjective(
+            sum(gamma[0,i]*opt_buy_book[i, premium] for i in range(option_num) ) - 
+            sum(delta[0,i]*opt_sell_book[i, premium] for i in range(option_num)),
+            GRB.MAXIMIZE
+        )
+    
+    # Solve the model
+    model.optimize()
+    
+    # Return results
+    if model.status == GRB.OPTIMAL:
+        profit = model.objVal
+        isMatch = any(delta[0,i].x > 0 for i in range(len(delta))) or any(gamma[0,j].x > 0 for j in range(len(gamma)))
+        return isMatch , profit  # Match format from training.py
+
+
+
+def mechanism_solver_combo(opt_buy_book : pd.DataFrame, opt_sell_book : pd.DataFrame, s1='S1', s2='S2', offset : bool = True, epsilon: bool = False, debug=0):
+	'''
+	opt_buy_book: pandas dataframe contains bid orders; specify whether code requires standarizing this variable
+	opt_sell_book: pandas dataframe contains ask orders;
+	s1: stock 1 name
+	s2: stock 2 name
+	order book: contains coefficients up to len(stock_list); call/put; strike; buy/sell; price (bid/ask)
+    offset: whether to offset the price by the offset value
+	debug: whether to debug
+	'''
+	buy_book_index = opt_buy_book.index
+	sell_book_index = opt_sell_book.index
+	sorted_columns_order = ['option1', 'option2','C=Call, P=Put',
+                'Strike Price of the Option Times 1000',
+                'transaction_type', 'B/A_price']
+	opt_buy_book = opt_buy_book[sorted_columns_order].to_numpy()
+	opt_sell_book = opt_sell_book[sorted_columns_order].to_numpy()
+	num_buy, num_sell, num_stock = len(opt_buy_book), len(opt_sell_book), len(opt_buy_book[0])-4
+	# add initial constraints
+
+	f_constraints = []
+	f_constraints.append(np.maximum(opt_buy_book[:, -4]*(np.concatenate(np.matmul(opt_buy_book[:, :-4], np.zeros((num_stock, 1))))-opt_buy_book[:, -3]), 0))
+	f_constraints.append(np.maximum(opt_buy_book[:, -4]*(np.concatenate(np.matmul(opt_buy_book[:, :-4], sys.maxsize*np.ones((num_stock, 1))))-opt_buy_book[:, -3]), 0))
+	g_constraints = []
+	g_constraints.append(np.maximum(opt_sell_book[:, -4]*(np.concatenate(np.matmul(opt_sell_book[:, :-4], np.zeros((num_stock, 1))))-opt_sell_book[:, -3]), 0))
+	g_constraints.append(np.maximum(opt_sell_book[:, -4]*(np.concatenate(np.matmul(opt_sell_book[:, :-4], sys.maxsize*np.ones((num_stock, 1))))-opt_sell_book[:, -3]), 0))
+	sub_obj = 1
+
+	try:
+		# prime problemsys
+		model = Model("match")
+		model.setParam('OutputFlag', False)
+		gamma = model.addVars(1, num_buy, ub=1) #sell to bid orders
+		delta = model.addVars(1, num_sell, ub=1) #buy from ask orders
+		L = model.addVars(1, 1, lb=-GRB.INFINITY, ub=GRB.INFINITY)
+		# constraint of 0
+		buy_sum = sum(delta[0,i]*g_constraints[0][i] for i in range(num_sell))
+		sell_sum = sum(gamma[0,i]*f_constraints[0][i] for i in range(num_buy))
+		model.addLConstr(sell_sum-buy_sum-L[0,0], GRB.LESS_EQUAL, 0)
+		# define obj
+		expense = sum(delta[0,i]*opt_sell_book[i, -1] for i in range(num_sell))
+		gain = sum(gamma[0,i]*opt_buy_book[i, -1] for i in range(num_buy))
+		model.setObjective(gain-expense-L[0,0], GRB.MAXIMIZE)
+
+		# sub problem
+		sub_model = Model("sub_match")
+		sub_model.setParam('OutputFlag', False)
+		M = 1000000
+		s = sub_model.addVars(1, num_stock)
+		f = sub_model.addVars(1, num_buy, lb=-GRB.INFINITY)
+		g = sub_model.addVars(1, num_sell)
+		I = sub_model.addVars(1, num_buy, vtype=GRB.BINARY)
+		for i in range(num_sell):
+			sub_model.addLConstr(opt_sell_book[i, -4]*(sum(opt_sell_book[i, j]*s[0,j] for j in range(num_stock))-opt_sell_book[i, -3])-g[0, i], GRB.LESS_EQUAL, 0)
+		for i in range(num_buy):
+			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])+M*(1-I[0, i])-f[0, i], GRB.GREATER_EQUAL, 0)
+			sub_model.addLConstr(M*I[0, i]-f[0, i], GRB.GREATER_EQUAL, 0)
+			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])+M*(1-I[0, i]), GRB.GREATER_EQUAL, 0)
+			sub_model.addLConstr(opt_buy_book[i, -4]*(sum(opt_buy_book[i, j]*s[0,j] for j in range(num_stock))-opt_buy_book[i, -3])-M*I[0, i], GRB.LESS_EQUAL, 0)
+
+		it = 0
+		start = timeit.default_timer()
+		while sub_obj > 0.0005:
+			# add newly generated constraint
+			buy_sum_new = sum(delta[0,i]*g_constraints[-1][i] for i in range(num_sell))
+			sell_sum_new = sum(gamma[0,i]*f_constraints[-1][i] for i in range(num_buy))
+			model.addLConstr(sell_sum_new-buy_sum_new-L[0,0], GRB.LESS_EQUAL, 0)
+			model.optimize()
+			# for v in model.getVars():
+			# 	print('%s %g' % (v.varName, v.x))
+			# save decision variables from prime problem
+			gamma_val = np.array([max(gamma[0, i].x, 0) for i in range(num_buy)])
+			delta_val = np.array([max(delta[0, i].x, 0) for i in range(num_sell)])
+			L_val = L[0,0].x
+			if debug == 2:
+				print(gamma_val)
+				print(delta_val)
+				print(L_val)
+
+			# define sub obj
+			sub_model.setObjective(sum(gamma_val[i]*f[0, i] for i in range(num_buy))-sum(delta_val[i]*g[0, i] for i in range(num_sell))-L_val, GRB.MAXIMIZE)
+			sub_model.optimize()
+			# for v in sub_model.getVars():
+			# 	print('%s %g' % (v.varName, v.x))
+			if debug > 0:
+				if it % 100 == 0:
+					print([s[0, i].x for i in range(num_stock)])
+					print('{}: objective is {} > 0'.format(it, sub_model.objVal))
+				if debug == 2:
+					for i in range(num_buy):
+						print('I:', I[0, i].x)
+						print('f:', f[0, i].x)
+					for i in range(num_sell):
+						print('g:', g[0, i].x)
+			# save decision variables from sub problem			
+			f_constraints.append(np.array([f[0, i].x for i in range(num_buy)]))
+			g_constraints.append(np.array([g[0, i].x for i in range(num_sell)]))
+			sub_obj = sub_model.objVal
+			it += 1
+
+		stop = timeit.default_timer()
+		time = stop - start
+		# print matching result
+		if debug == 1:
+			revenue = 0
+			for i in range(num_buy):
+				if gamma[0, i].x > 0:
+					revenue += gamma[0,i].x * opt_buy_book[i, -1]
+					print('Sell {} to {}({}{}+{}{},{}) at bid price {}'.format(round(gamma[0,i].x, 4), 'C' if opt_buy_book[i, -4]==1 else 'P', \
+						opt_buy_book[i, 0], s1, opt_buy_book[i, 1], s2, opt_buy_book[i, -3], opt_buy_book[i, -1]))
+			for i in range(num_sell):
+				if delta[0, i].x > 0:
+					revenue -= delta[0,i].x * opt_sell_book[i, -1]
+					print('Buy {} from {}({}{}+{}{},{}) at ask price {}'.format(round(delta[0,i].x, 4), 'C' if opt_sell_book[i, -4]==1 else 'P', \
+						opt_sell_book[i, 0], s1, opt_sell_book[i, 1], s2, opt_sell_book[i, -3], opt_sell_book[i, -1]))
+			print('Revenue at T0 is {}; L is {}; Objective is {} = {}'.format(round(revenue,2), round(L[0,0].x, 2), round(revenue-L[0,0].x, 2), round(model.objVal, 2)))
+	except GurobiError as e:
+		print('Error code ' + str(e.errno) + ": " + str(e))
+	except AttributeError:
+		print('Encountered an attribute error')
+	
+	if model is None:
+		return time, 0, 0
+	isMatch = any(delta[0,i].x > 0 for i in range(len(delta))) or any(gamma[0,j].x > 0 for j in range(len(gamma)))
+	matched_stock = {'buy_book_index': None, 'sell_book_index': None}
+	matched_stock['buy_book_index'] = [buy_book_index[i] for i in range(len(gamma)) if gamma[0, i].x > 0]
+	matched_stock['sell_book_index'] = [sell_book_index[i] for i in range(len(delta)) if delta[0, i].x > 0]
+	return time, model.NumConstrs, model.objVal, isMatch, matched_stock
